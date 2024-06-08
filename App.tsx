24d0ae12bb4f4b7d186bfe461dade02a7b562423
Shelja Sharma <sheljasharma@pixelsoftwares.co>
2024-06-08 13:38:28 +0530

    main commit Main-C2

--- App.tsx
+++ App.tsx
@@ -81,7 +81,7 @@ function App(): React.JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+          Now working fine
           </Section>
           <LearnMoreLinks />
         </View>
