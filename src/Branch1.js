081f0703a625ea0e65a93610561ceb7ece474188
Shelja Sharma <sheljasharma@pixelsoftwares.co>
2024-06-08 13:10:40 +0530

    Branch1 ScreenAdded -- B1- C1

---
+++ src/Branch1.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Branch1 = () => {
+  return (
+    <View>
+      <Text>Branch1</Text>
+    </View>
+  )
+}
+
+export default Branch1
